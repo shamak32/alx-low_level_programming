6fb2cdb8ebf2952bbb4e9f6eb7ecc1ba49281250
shamak32 <benhilary51@gmail.com>
2023-10-23 17:35:10 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -16,7 +16,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 listint_t *new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 	{
-	return NULL; // Allocation failed, return NULL
+	return (NULL);
 	}
 
 	new_node->n = n;
